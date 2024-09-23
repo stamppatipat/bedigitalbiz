9e638e6dcdc0499893a0bf4f9245de193dab1110
BeDigitalBiz <bedigitalbiz.info@gmail.com>
2024-09-23 18:15:29 +0700

    Update index.tsx

--- app/components/Business/index.tsx
+++ app/components/Business/index.tsx
@@ -11,7 +11,7 @@ const Business = () => {
 
                 <div className='col-span-6 flex flex-col justify-center'>
                     <h2 className='text-midnightblue text-4xl sm:text-5xl font-semibold text-center lg:text-start lh-143'>Be Digital Biz มอบประสบการณ์การทํางานแบบไฮบริดที่ราบรื่น</h2>
-                    <h3 className='text-black text-lg font-normal text-center lg:text-start lh-173 opacity-75 pt-3'>การเชื่อมต่อที่เชื่อถือได้และปลอดภัยสามารถเข้าถึงได้ไม่ว่าคุณจะอยู่ที่ไหน วางใจในเทคโนโลยี Meraki ในสํานักงาน ที่บ้าน หรือออกนอกบ้านบนถนนเปิด</h3>
+                    <h3 className='text-black text-lg font-normal text-center lg:text-start lh-173 opacity-75 pt-3'>การเชื่อมต่อที่เชื่อถือได้และปลอดภัยสามารถเข้าถึงได้ไม่ว่าคุณจะอยู่ที่ไหน วางใจในเทคโนโลยี Meraki ในสํานักงาน ที่บ้าน หรือออกนอกบ้านบนถนน</h3>
                     {/* <Link href={'/'} className="text-electricblue text-lg font-medium flex gap-2 pt-4 mx-auto lg:mx-0">
                         Learn more <Image src="/assets/people/arrow-right.svg" alt="arrow-right" width={24} height={24} />
                     </Link> */}
